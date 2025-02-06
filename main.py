fa39efc9862b2950a06fc7df067888a285c28fd8
Mert Yaylaci <151733368+mertyaylacigit@users.noreply.github.com>
2025-02-06 23:48:57 +0000

    save until "Upload Image"

--- main.py
+++ main.py
@@ -1,99 +1,40 @@
 import discord, os, json
 from discord.ext import commands
-from discord.ui import Button, View
+from discord.ui import Modal, TextInput, Button
+from views.base_view import BaseView
+from config import TOKEN, WELCOME_CHANNEL_ID, CATEGORY_ID
 from replit import db
-
-CATEGORY_ID = 1329571928482250835  # category "SupportAmar" in order to only take supportAmar request into account
-WELCOME_CHANNEL_ID = 1329571928951754823
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.reactions = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-print(db, db.keys())
-db_data = {key: db[key] for key in db.keys()}
-print(json.dumps(db_data))
 
-class VerifyView(View):
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if isinstance(message.channel, discord.Thread) and message.channel.type == discord.ChannelType.private_thread:
+        if message.channel.parent:
+            parent_channel = message.channel.parent
+            if parent_channel.category and parent_channel.category.id != CATEGORY_ID:
+                return
+        # Check if the message has an attachment (image/video)
+        if message.attachments:
+            allowed_file_types = ["image", "video"]
 
-    def __init__(self):
-        super().__init__(timeout=None)
+            # Check if the attachment is an image or video
+            for attachment in message.attachments:
+                if any(file_type in attachment.content_type for file_type in allowed_file_types):
+                    user = message.author
+                    thread = message.channel
+                    print(f" It is a {attachment.content_type} file.")
+                    return  
 
-    @discord.ui.button(label="Verify",
-                       style=discord.ButtonStyle.green,
-                       custom_id="verify_button")
-    async def verify(self, interaction: discord.Interaction, button: Button):
-        guild = interaction.guild
-        user = interaction.user
-        channel = guild.get_channel(WELCOME_CHANNEL_ID)
-
-        # check if the user already has an open thread
-        existing_thread = discord.utils.get(
-            channel.threads, name=f"{user.name.lower()}-verification")
-        if existing_thread:
-            await interaction.response.send_message(
-                f"❌ You already have an open ticket: {existing_thread.mention}",
-                ephemeral=True)
-            return
-
-        # create a private thread for the user
-        thread = await channel.create_thread(
-            name=f"{user.name.lower()}-verification",
-            type=discord.ChannelType.private_thread,
-            invitable=False  # Ensures only invited users can join
-        )
-        # Add the user to the thread
-        await thread.add_user(user)
-
-        # Send a message in the thread
-        await thread.send(
-            f"Hello {user.mention}! Please type your Epic Games ID below. Once entered, I'll ask you to confirm it!"
-        )
-
-        # Respond to the user in the main channel
-        await interaction.response.send_message(
-            f"✅ Your private ticket has been created: {thread.mention}",
-            ephemeral=True)
-
-
-class ConfirmationView(View):
-
-    def __init__(self, thread, user):
-        super().__init__(timeout=None)
-        self.thread = thread
-        self.user = user
-
-    @discord.ui.button(label="Yes",
-                       style=discord.ButtonStyle.green,
-                       custom_id="confirm_yes")
-    async def confirm_yes(self, interaction: discord.Interaction,
-                          button: Button):
-        if interaction.user != self.user:
-            await interaction.response.send_message(
-                "You are not authorized to respond here.", ephemeral=True)
-            return
-
-        await interaction.response.send_message(
-            "✅ Thank you! Your Epic Games ID has been confirmed and saved.",
-            ephemeral=True)
-        db[f"epic_{self.user.id}"] = interaction.message.content.split(
-            "Is ")[1].split(" your Epic Games ID?")[0]
-        print(db)
-        await self.thread.send("✅ Your Epic Games ID has been saved.")
-
-    @discord.ui.button(label="No, enter again",
-                       style=discord.ButtonStyle.red,
-                       custom_id="confirm_no")
-    async def confirm_no(self, interaction: discord.Interaction,
-                         button: Button):
-        if interaction.user != self.user:
-            await interaction.response.send_message(
-                "You are not authorized to respond here.", ephemeral=True)
-            return
-
-        await interaction.response.send_message(
-            "❌ Please enter your Epic Games ID again below.", ephemeral=True)
+        # If the message is not an image or video, delete it
+        await message.delete()
+        await message.channel.send(f"❌ {message.author.mention}, nur Bilder oder Videos sind erlaubt!", delete_after=5)
 
 
 @bot.event
@@ -102,41 +43,156 @@ async def on_ready():
     channel = bot.get_channel(WELCOME_CHANNEL_ID)
 
     if channel:
-        # Clear all messages in the channel
-        await channel.purge(limit=100)
+        # Purge all threads in the channel (only for development)
+        for thread in channel.threads:
+            try:
+                await thread.delete()
+                print(f"🗑️ Thread gelöscht: {thread.name}")
+            except Exception as e:
+                print(f"❌ Konnte den Thread {thread.name} nicht löschen: {e}")
+
+        await channel.purge()
+
         embed = discord.Embed(
             title="SupportAmar Ticketsystem",
-            description=
-            "Hier ....  \n Drücke auf Verifizieren, um zu verifieren, dass du Amar supportest!",
-            color=discord.Color.blue())
-        view = VerifyView()
-
+            description="Hier kannst du verifizieren, dass du Amar supportest. \nDrücke auf **Verifizieren**, um zu beginnen!",
+            color=discord.Color.blue()
+        )
+        view = Welcome2VerifyView()
         await channel.send(embed=embed, view=view)
 
 
-@bot.event
-async def on_message(message):
-    # Ignore bot messages
-    if message.author.bot:
-        return
+class Welcome2VerifyView(BaseView):
+    def __init__(self):
+        super().__init__(timeout=None)
 
-    # Check if the message is in a private thread
-    if isinstance(
-            message.channel, discord.Thread
-    ) and message.channel.type == discord.ChannelType.private_thread:
-        # check if message is in the supportAmar category
-        if message.channel.parent:
-            parent_channel = message.channel.parent
-            if parent_channel.category and parent_channel.category.id != CATEGORY_ID:
-                print("rip", parent_channel.category.id)
-                return
-        user = message.author
-        thread = message.channel
-        epic_id = message.content.strip()
+    @discord.ui.button(label="Verifizieren", style=discord.ButtonStyle.green, custom_id="verify_button")
+    async def verify(self, interaction: discord.Interaction, button: Button):
+        guild = interaction.guild
+        user = interaction.user
+        channel = guild.get_channel(WELCOME_CHANNEL_ID)
 
-        # Prompt the user to confirm their Epic Games ID
-        await thread.send(f"Is `{epic_id}` your Epic Games ID?",
-                          view=ConfirmationView(thread, user))
+        existing_thread = discord.utils.get(channel.threads, name=f"{user.name.lower()}-verifizierung")
+        if existing_thread:
+            await interaction.response.send_message(
+                f"❌ Du hast bereits ein offenes Ticket: {existing_thread.mention}",
+                ephemeral=True
+            )
+            return
+
+        thread = await channel.create_thread(
+            name=f"{user.name.lower()}-verifizierung",
+            type=discord.ChannelType.private_thread,
+            invitable=False
+        )
+        await thread.add_user(user)
+        embed = discord.Embed(
+            title="Gib deine Epic Games ID ein",
+            description="Drücke auf den Button unten, um deine Epic Games ID einzugeben.",
+            color=discord.Color.blue()
+        )
+        view = Verify2EnterIDView(thread, user)
+
+        await thread.send(embed=embed, view=view)
+
+        await interaction.response.send_message(f"✅ Dein privates Ticket wurde erstellt: {thread.mention}", ephemeral=True)
 
 
-bot.run(os.getenv('TOKEN'))
+class Verify2EnterIDView(BaseView):
+    def __init__(self, thread, user):
+        super().__init__(timeout=None)
+        self.thread = thread
+        self.user = user
+
+    @discord.ui.button(label="Epic Games ID eingeben", style=discord.ButtonStyle.blurple, custom_id="enter_epic_id")
+    async def enter_epic_id(self, interaction: discord.Interaction, button: Button):
+        if interaction.user != self.user:
+            await interaction.response.send_message("Du bist nicht berechtigt, dies zu tun.", ephemeral=True)
+            return
+
+        await interaction.response.send_modal(EpicIDModal(self.thread, self.user))
+
+
+class EnterID2ConfirmView(BaseView):
+    def __init__(self, thread, user):
+        super().__init__(timeout=None)
+        self.thread = thread
+        self.user = user
+
+    @discord.ui.button(label="Ja", style=discord.ButtonStyle.green, custom_id="confirm_yes")
+    async def confirm_yes(self, interaction: discord.Interaction, button: Button):
+        if interaction.user != self.user:
+            await interaction.response.send_message("Du bist nicht berechtigt, dies zu tun.", ephemeral=True)
+            return
+        # Extract the embed description
+        embed = interaction.message.embeds[0]  # Get the first embed
+        if embed and embed.description:
+            # Extract the Epic Games ID from the description
+            epic_id = embed.description.split("`")[1]  # Extract the text between backticks
+            db[f"epic_{self.user.id}"] = epic_id
+        await interaction.response.send_message("✅ Danke! Deine Epic Games ID wurde bestätigt und gespeichert.", ephemeral=True)
+        #await self.thread.send("✅ Deine Epic Games ID wurde gespeichert.")
+
+        # Disable the buttons after clicking
+        await self.disable_buttons(interaction)
+
+        # next step: proof code registration
+        embed = discord.Embed(
+            title="Creator Code Amar eingetragen?",
+            description=(
+                "Bitte lade ein Beweisbild hoch, das zeigt, dass du den Creator Code benutzt hast:\n\n🔹 **Das Bild sollte Folgendes zeigen:**\n"
+                "1️⃣ Dein **Epic Games Konto**.\n"
+                "2️⃣ Den Artikel im **Fortnite Shop**, bei dem der Creator Code sichtbar ist.\n\n"
+                "🔸 **So lädst du das Bild hoch:**\n"
+                "- Drücke unten auf das **+** Symbol oder ziehe das Bild in den Thread.\n"
+                "- Stelle sicher, dass das Bild klar und lesbar ist.\n\n"
+                "**Beachte:** Nur Bilder, die über Discords Upload-Funktion gesendet werden, werden akzeptiert."
+                "➡️ **Das Bild sollte ungefähr so aussehen:**"
+            ),
+            color=discord.Color.blue()
+        )
+        embed.set_image(url="https://cdn.discordapp.com/attachments/894683986868203551/1337204328628486186/image.png?ex=67a69814&is=67a54694&hm=e2e5a97814f60b2d9d5b62d2bfb34be0350f14185ce9982f43ce6ff18c55bfcb&")
+        embed.set_footer(text="Vielen Dank für deinen Support! ❤️")
+        await self.thread.send(embed=embed)
+
+    @discord.ui.button(label="Nein, erneut eingeben", style=discord.ButtonStyle.red, custom_id="confirm_no")
+    async def confirm_no(self, interaction: discord.Interaction, button: Button):
+        if interaction.user != self.user:
+            await interaction.response.send_message("Du bist nicht berechtigt, dies zu tun.", ephemeral=True)
+            return
+        await self.disable_buttons(interaction)
+
+        await interaction.response.send_modal(EpicIDModal(self.thread, self.user))
+
+
+class EpicIDModal(Modal):
+    def __init__(self, thread, user):
+        super().__init__(title="Gib deine Epic Games ID ein")
+        self.thread = thread
+        self.user = user
+        self.text = TextInput(
+            label="Gib deine Epic Games ID ein",
+            placeholder="z.B. EpicGamer123",
+            required=True
+        )
+        self.add_item(self.text)
+
+    async def on_submit(self, interaction: discord.Interaction):
+        await interaction.response.defer()
+
+        embed = discord.Embed(
+            title="Bestätige deine Eingabe",
+            description=f"Ist `{self.text.value}` deine Epic Games ID?",
+            color=discord.Color.blue()
+        )
+        view = EnterID2ConfirmView(self.thread, self.user)
+
+        await self.thread.send(embed=embed, view=view)
+
+
+
+
+    
+
+
+bot.run(TOKEN)
